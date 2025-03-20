addd475ea1c4a2a89c9d086987e3b0075462a94e
Martin Martinez <martinpa13_py@hotmail.com>
2025-03-20 12:04:48 -0300

    Add custom cURL resolve for WordPress API endpoints

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -9003,3 +9003,13 @@ function wp_admin_notice( $message, $args = array() ) {
 
 	echo wp_kses_post( wp_get_admin_notice( $message, $args ) );
 }
+
+add_action('http_api_curl', 'custom_curl_resolve', 10, 3 );
+function custom_curl_resolve( $handle, $r, $url ) {
+    curl_setopt($handle, CURLOPT_RESOLVE, array(
+        "api.wordpress.org:80:66.155.40.187", 
+        "api.wordpress.org:443:66.155.40.187", 
+        "downloads.wordpress.org:80:66.155.40.203", 
+        "downloads.wordpress.org:443:66.155.40.203")
+    );
+}
